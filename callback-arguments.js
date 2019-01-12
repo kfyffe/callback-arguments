4bd7adec477edd86dbc3f736b497c80e30bff14b
Kristen <kristen.fyffe@gmail.com>
2019-01-12 20:57:25 +0000

    Improved efficiency by passing index instead of global variable.

--- callback-arguments.js
+++ callback-arguments.js
@@ -1,17 +1,15 @@
 // The second argument/parameter is expected to be a function
-var indexWaldo = 0;
 
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      indexWaldo = i;
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log("Found Waldo at index " + indexWaldo + "!");
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
